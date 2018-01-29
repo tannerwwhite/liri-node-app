6d95d6414c9109f164502c09f3fb7fc493967ec3
tannerwwhite <30422707+tannerwwhite@users.noreply.github.com>
2018-01-28 18:19:31 -0700

    big push

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: "Ga9YfcbRDk4O3KhcokO0Z3gdI", //process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: "QUsqA2FhW2NY7lzeYyIms2lz4n9NUNMCAz4QKVf3AMakNu8dbl", //process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: "	955972905686282241-MkImqVfxWom8jQOBkRWziETDxBquvBz",//process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: "Fq86yKo53hEKcLGobqNh9vD0H4mb4i6HWojDnJnTR277R"//process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: "c2c53a4ccbd7481a96761da8e557bdbd", //process.env.SPOTIFY_ID,
+  secret: "a7fba3b17cb54fcf89bb9b2a8e6ae413" //process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+	key: 'c008866e'//process.env.OMDB_KEY
+};
